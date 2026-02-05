01bf08250140262890ccbdfe795b1f350307d517
Abdul <Abdul@DESKTOP-EOR8I6B>
2026-02-05 15:09:03 +0500

    Create Fuction For Plot_region_gdp and plot_year_gdp_line

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,4 +1,6 @@
 
+
+
 import matplotlib.pyplot as plt
 
 
@@ -23,6 +25,34 @@ def plot_region_gdp(filtered_data, config):
     plt.figure(figsize=(10, 6))
     plt.bar(countries, values)
     plt.title(f"Region-wise GDP ({config.get('region')} - {year})")
+    plt.xlabel("Country")
+    plt.ylabel("GDP Value")
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_year_gdp_line(filtered_data, config):
+    """
+    Line graph: GDP comparison of countries for a given year
+    """
+    year = str(config.get("year"))
+
+    if not filtered_data:
+        print("No data available for year GDP line plot.")
+        return
+
+    countries = list(map(lambda x: x["Country Name"], filtered_data))
+    values = list(
+        map(
+            lambda x: float(x[year]) if x.get(year) not in ("", None) else 0,
+            filtered_data
+        )
+    )
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(countries, values, marker='o')
+    plt.title(f"Year-specific GDP ({year})")
     plt.xlabel("Country")
     plt.ylabel("GDP Value")
     plt.xticks(rotation=45, ha="right")
